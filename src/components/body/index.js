07812135f4b25404f351e2fbb6c521a5c6b93798
Mimimoha <mohamedm1987@gmail.com>
2022-11-25 14:49:07 -0600

    final

--- src/components/body/index.js
+++ src/components/body/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import "./body.css";
 import About from "./about/index";
 import Projects from "./projects/index";
-import Resume from "./resume/index";
 import Contact from "./contact/index";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
@@ -15,7 +14,6 @@ function Body() {
         <Routes>
           <Route path='/' element={<About />} />
           <Route path='/projects' element={<Projects />} />
-          {/* <Route path='/resume' element={<Resume />} /> */}
           <Route path='/contact' element={<Contact />} />
         </Routes>
       </BrowserRouter>
